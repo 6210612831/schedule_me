ee3a9d88d920baa4478c4261fa1e4cab349704c7
Pattana Sapphanachai <6210612831@student.tu.ac.th>
2022-03-29 17:07:05 +0700

    Add design to register page

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
-    path("login", views.login, name="login"),
+    #path("login", views.login, name="login"),
     path("login_check", views.login_check, name="login_check"),
     path("register", views.register_view, name="register"),
     path("confirm_register", views.register, name="confirm_register"),
